c8b35468aafe0821497fb016bc666485d54af6f6
Benjamin Crom <benjamincrom@gmail.com>
2017-02-16 11:34:44 -0500

    legal moves

--- test_scrabble_game.py
+++ test_scrabble_game.py
@@ -38,9 +38,33 @@ def test_board_moves_score():
                                '15_______________')
 
 
-'''
-def test_bingo
+def test_bingo():
+    game = scrabble_game.ScrabbleGame(4)
+    game.place_word('BAKER', ('h', 8), False, True)
+    game.place_word('AKELAKE', ('l', 9), True, True)
 
+    import pdb; pdb.set_trace()  # breakpoint 8e011190 //
+    assert game.player_score_list_list == [[12], [13], [17], []]
+    assert game.move_number == 3
+    assert len(game.tile_bag) == 72
+    assert str(game.board) == ('  abcdefghijklmno\n'
+                               '1 _______________\n'
+                               '2 _______________\n'
+                               '3 _______________\n'
+                               '4 _______________\n'
+                               '5 ______P________\n'
+                               '6 ______I________\n'
+                               '7 ______L________\n'
+                               '8 ______ESCRAB___\n'
+                               '9 ________O______\n'
+                               '10________D______\n'
+                               '11________I______\n'
+                               '12________N______\n'
+                               '13________G______\n'
+                               '14_______________\n'
+                               '15_______________')
+
+'''
 def test_itersect_words_regular
 
 def test_intersetct_corner

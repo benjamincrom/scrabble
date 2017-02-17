127b36e3592d99008c273ab1002bc06c5ec07d2e
Benjamin Crom <benjamincrom@gmail.com>
2017-02-17 14:04:12 -0500

    legal moves

--- scrabble_recover.py
+++ scrabble_recover.py
@@ -8,7 +8,7 @@ def read_input_file(input_filename):
         input_str = filehandle.read()
 
     input_dict = json.loads(input_str)
-    board_character_list_list = input_dict['board']
+    board_character_array = input_dict['board']
     player_scores_list_list = input_dict['scores']
 
     return board_character_array, player_scores_list_list
@@ -20,11 +20,12 @@ board_character_array, player_scores_list_list = read_input_file(
 
 num_players = len(player_scores_list_list)
 game = scrabble_game.ScrabbleGame(num_players)
+game.player_rack_list = [[] for _ in range(num_players)]
+game.tile_bag = []
 
 for row_number, row in enumerate(board_character_array):
     for column_number, letter in enumerate(row):
         if letter:
             column_letter = chr(ord('a') + column_number)
-            game.board[column_letter, row_number] = scrabble_board.ScrabbleTile(
-                letter
-            )
+            this_location = (column_letter, row_number + 1)
+            game.board[this_location] = scrabble_board.ScrabbleTile(letter)

43cbd6d965074ba91475de782440bd0affaf3483
Benjamin Crom <benjamincrom@gmail.com>
2017-03-02 18:33:55 -0500

    Final docs

--- test_scrabble_game.py
+++ test_scrabble_game.py
@@ -483,5 +483,4 @@ def test_get_best_move():
 
     score, move_tuple = helpers.get_best_move(game)
 
-    assert (score, move_tuple) == (16, (('l', 9), 'ANDS', True))
-    
+    assert (score, move_tuple) == (81, (('a', 4), 'DOPANTS', False))

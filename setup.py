869c268c963c9bd2c1139d9077cc43bb0a2a347d
Benjamin Crom <benjamincrom@gmail.com>
2017-03-05 00:16:24 -0500

    home

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='scrabble',
+      version='0.1',
+      description='Scrabble Game with move recovery and best move analysis',
+      url='https://github.com/benjamincrom/scrabble',
+      author='Benjamin B. Crom',
+      author_email='benjamincrom@gmail.com',
+      license='MIT',
+      packages=['scrabble'],
+      zip_safe=False)

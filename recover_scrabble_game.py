8909abdeeac120b72207167db21f94727022ae01
Benjamin Crom <benjamincrom@gmail.com>
2017-03-04 23:40:20 -0500

    home

--- recover_scrabble_game.py
+++ recover_scrabble_game.py
@@ -5,11 +5,11 @@ Usage:
     See sample_input_files/ for examples of correctly formatted input files
 """
 import sys
-import helpers
+import scrabble
 
 if __name__ == '__main__':
     if len(sys.argv) == 2:
-        for move_list in helpers.recover_game(sys.argv[1]):
+        for move_list in scrabble.helpers.recover_game(sys.argv[1]):
             print('{}\n\n'.format(move_list))
     else:
         print('Usage: python3 recover_scrabble_game.py [INPUT_FILENAME]')

171de373a08f8b24d9bbb243cf845318d0cafb44
Benjamin Crom <benjamincrom@gmail.com>
2017-03-05 11:30:37 -0500

    Version 0.3

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='scrabble',
-      version='0.2',
+      version='0.3',
       description='Scrabble Game with move recovery and best move analysis',
       url='https://github.com/benjamincrom/scrabble',
       author='Benjamin B. Crom',
